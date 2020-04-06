7297801486dd25e9c0061336b47037ddc54adf9d
AlecHubacher <61433423+AlecHubacher@users.noreply.github.com>
2020-04-06 02:24:03 -0500

    Add files via upload
    Fills all chests with items depending on tier of chest

---
+++ AutoFillChests/Main.java
@@ -0,0 +1,13 @@
+package me.trumpet.autofillchests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import me.trumpet.autofillchests.commands.*;
+
+public class Main extends JavaPlugin{
+	
+	public void onEnable()
+	{
+		new AutoFillChestsCommands(this);
+	}
+
+}
