72d18eee912cb314c8158c42e6de662be26382d0
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-06-15 13:59:07 -0500

    Add files via upload

--- MCGame/src/main/java/mproject/dontknow/Dontknow.java
+++ MCGame/src/main/java/mproject/dontknow/Dontknow.java
@@ -1,7 +1,15 @@
 package mproject.dontknow;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public final class Dontknow extends JavaPlugin implements Listener {
 
@@ -15,6 +23,7 @@ public final class Dontknow extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
 
+        instance = this;
         new TruceFeature(this);
         getServer().getPluginManager().registerEvents(this, this);
 
@@ -23,5 +32,128 @@ public final class Dontknow extends JavaPlugin implements Listener {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+
+    boolean isSwap = false;
+    int count = 0;
+    int track = 0;
+    int swapTrack = 0;
+    int tpTrack = 0;
+    Player dontSwapMultiple;
+
+    //add a cooldown of some sort
+    @EventHandler
+    public void truceTeleport(PlayerInteractEvent e)
+    {
+        ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
+        Player p = e.getPlayer();
+        if(isSwap && (!dontSwapMultiple.equals(p)) && (item.equals(TruceFeature.swapPlacesReceiver) || item.equals(TruceFeature.swapPlacesSender)) && TruceFeature.truceMap.get(p).contains(dontSwapMultiple) && !(e.getAction() == Action.LEFT_CLICK_AIR) && !(e.getAction() == Action.LEFT_CLICK_BLOCK))
+        {
+            p.sendMessage("inside else if checking for other person clicking swap");
+            String[] name = item.getItemMeta().getDisplayName().split(" ");
+            String pName = name[0].substring(2);
+            for(int i = 0; i < TruceFeature.truceMap.get(p).size(); i++)
+            {
+                if(TruceFeature.truceMap.get(p).get(i).getDisplayName().equalsIgnoreCase(pName))
+                {
+                    TruceFeature.cooldownMap.get(dontSwapMultiple).set(i + i + 1, false);
+                    swapTrack = 0;
+                    int inum = i;
+                    Location truceLocation = TruceFeature.truceMap.get(p).get(i).getLocation();
+                    Location pLocation = p.getLocation();
+                    p.teleport(truceLocation);
+                    TruceFeature.truceMap.get(p).get(i).getPlayer().teleport(pLocation);
+                    isSwap = false;
+
+                    new BukkitRunnable()
+                    {
+
+                        @Override
+                        public void run() {
+                            swapTrack++;
+                            if(swapTrack == 2)
+                            {
+                                swapTrack = 0;
+                                TruceFeature.cooldownMap.get(dontSwapMultiple).set(inum + inum + 1, true);
+                                Bukkit.getServer().getScheduler().cancelTasks(getInstance());
+                            }
+                        }
+                    }.runTaskTimer(getInstance(), 0, 40);
+                }
+            }
+        }
+        else if(isSwap && p == dontSwapMultiple)
+        {
+            p.sendMessage("You must wait for a response");
+        }
+        else if((item.equals(TruceFeature.swapPlacesReceiver) || item.equals(TruceFeature.swapPlacesSender)) && isSwap == false && !(e.getAction() == Action.LEFT_CLICK_AIR) && !(e.getAction() == Action.LEFT_CLICK_BLOCK))
+        {
+            dontSwapMultiple = p;
+            String[] name = item.getItemMeta().getDisplayName().split(" ");
+            String pName = name[0].substring(2);
+            for(int i = 0; i < TruceFeature.truceMap.get(p).size(); i++)
+            {
+                if(TruceFeature.truceMap.get(p).get(i).getDisplayName().equalsIgnoreCase(pName) && TruceFeature.cooldownMap.get(p).get(i + i + 1))
+                {
+                    //TruceFeature.cooldownMap.get(p).set(i + i + 1, false);
+                    track = 0;
+                    Player otherPlayer = TruceFeature.truceMap.get(p).get(i).getPlayer();
+                    otherPlayer.sendMessage(p + " wants to swap places. Right click the swap item to do so.");
+                    otherPlayer.sendMessage("If you don't want to swap places, ignore this");
+                    isSwap = true;
+                }
+            }
+            //Player otherPlayer = TruceFeature.truceMap.get(p).get(0).getPlayer();
+
+            new BukkitRunnable()
+            {
+
+                @Override
+                public void run() {
+                    track++;
+                    if(track == 2)
+                    {
+                        track = 0;
+                        isSwap = false;
+                        Bukkit.getServer().getScheduler().cancelTasks(getInstance());
+                    }
+                }
+            }.runTaskTimer(getInstance(), 0, 100);
+        }
+        //p.sendMessage("entering if");
+        else if((item.equals(TruceFeature.teleporterReceiver) || item.equals(TruceFeature.teleporterSender)) && !(e.getAction() == Action.LEFT_CLICK_AIR) && !(e.getAction() == Action.LEFT_CLICK_BLOCK))
+        {
+            String[] name = item.getItemMeta().getDisplayName().split(" ");
+            String pName = name[0].substring(2);
+            for(int i = 0; i < TruceFeature.truceMap.get(p).size(); i++)
+            {
+                if(TruceFeature.truceMap.get(p).get(i).getDisplayName().equalsIgnoreCase(pName) && TruceFeature.cooldownMap.get(p).get(i + i))
+                {
+                    //make cooldown for this
+                    int in = i;
+                    TruceFeature.cooldownMap.get(p).set(i + i, false);
+                    tpTrack = 0;
+                    Location tpLocation = TruceFeature.truceMap.get(p).get(i).getLocation();
+                    p.teleport(tpLocation);
+
+                    new BukkitRunnable()
+                    {
+
+                        @Override
+                        public void run() {
+                            tpTrack++;
+                            if(tpTrack == 2)
+                            {
+                                tpTrack = 0;
+                                TruceFeature.cooldownMap.get(p).set(in + in, true);
+                                Bukkit.getServer().getScheduler().cancelTasks(getInstance());
+                            }
+                        }
+                    }.runTaskTimer(getInstance(), 0, 40);
+                }
+            }
+        }
+
     }
 }
--- MCGame/src/main/java/mproject/dontknow/TruceFeature.java
+++ MCGame/src/main/java/mproject/dontknow/TruceFeature.java
@@ -5,25 +5,25 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class TruceFeature implements CommandExecutor
 {
 
-    private Dontknow plugin;
-    private String soloPlayer;
-    private List<Player> playerList = (List<Player>) plugin.getServer().getWorld("World").getPlayers();
+    public static Dontknow plugin;
+    public static Player soloPlayer;
+    boolean canSwap = true;
+    boolean canTP = true;
+    //private List<Player> playerList = Bukkit.getServer().getWorld("World").getPlayers();
+    //private List<Player> playerList = Bukkit.getServer().getOnlinePlayers();
+    //private List<Player> playerList;
+    public static ArrayList<Player> playerList = new ArrayList<>();
+
     //private Collection<Player> playerList = plugin.getServer().getOnlinePlayers();
     //int playerAmount = plugin.getServer().getWorld("World").getPlayers().size();
 
@@ -39,87 +39,198 @@ public class TruceFeature implements CommandExecutor
     ArrayList<String> p2 = new ArrayList<>(2);
     ArrayList<String> p3 = new ArrayList<>(2);
  */
-    HashMap<Player, ArrayList<Player>> truceMap = new HashMap<>();
-
+    public static HashMap<Player, ArrayList<Player>> truceMap = new HashMap<>();
+    public static HashMap<Player, ArrayList<Boolean>> cooldownMap = new HashMap<>();
+/*
     Material teleporter = Material.MUSIC_DISC_13;
     Material swapPlaces = Material.MUSIC_DISC_11;
+  */
+    public static ItemStack teleporterReceiver = new ItemStack(Material.MUSIC_DISC_13);
+    public static ItemStack swapPlacesReceiver = new ItemStack(Material.EMERALD);
+
+    public static ItemStack teleporterSender = new ItemStack(Material.MUSIC_DISC_13);
+    public static ItemStack swapPlacesSender = new ItemStack(Material.EMERALD);
+    /*
+    public void makePlayerList()
+    {
+        for(Player p : Bukkit.getOnlinePlayers()getServer().getOnlinePlayers())
+        {
+            playerList.add(p);
+            System.out.println("Each player: " + p.getName() + " within foreach loop creating playerList");
+        }
+    }
+
+     */
 
 
 
+    ArrayList<Player> temp = new ArrayList<>();
 
     @Override
     public boolean onCommand(CommandSender send, Command cmd, String label, String[] args) {
         // make this outside of the onCommand method, otherwise it will be called every time a command is passed through
+        /*
+        for (Player p: p) {
+
+        }
+         */
+        Player sender = (Player) send;
+            for(Player p : Bukkit.getOnlinePlayers()/*getServer().getOnlinePlayers()*/)
+            {
+                if(playerList.contains(p))
+                {
+                    break;
+                }
+                else {
+                    playerList.add(p);
+                    p.sendMessage("Each player: " + p.getName() + " within foreach loop creating playerList");
+                }
+            }
+/*
         if(cmd.getName().equalsIgnoreCase("Setsolo"))
         {
-            if(playerList.contains(args[0]))
+            if(soloPlayer == null)
             {
-                soloPlayer = args[0];
                 for(int i = 0; i < playerList.size(); i++)
                 {
-                    if(playerList.get(i).toString().equalsIgnoreCase(soloPlayer))
+                    if(playerList.get(i).getName().equalsIgnoreCase(args[0]))
                     {
-                        playerList.remove(playerList.get(i));
+                        soloPlayer = playerList.get(i);
+                        //playerList.remove(playerList.get(i));
+                        Bukkit.broadcastMessage("This is the soloPlayer: " + soloPlayer);
                     }
                     else
                     {
+                        Bukkit.broadcastMessage(playerList.get(i).getName() + " has been added to the truceMap");
                         truceMap.put(playerList.get(i), new ArrayList<>());
+                        cooldownMap.put(playerList.get(i), new ArrayList<>());
                     }
                 }
-            }
-        }
-
-        Player sender = (Player) send;
-        if(cmd.getName().equalsIgnoreCase("Truce"))
-        {
-            if(playerList.contains(args[0]))
-            {
-                int pNum = playerList.indexOf(args[0]);
-                Player receiver = playerList.get(pNum);
-
-                if(args.length < 2)
-                {
-                    receiver.sendMessage(sender.getName() + " wishes to truce with you");
-                    receiver.sendMessage("/truce " + sender.getName() + " accept to accept their request");
-                    receiver.sendMessage("/truce " + sender.getName() + " deny to deny their request");
-                }
-                else if(args.length == 2)
-                {
-                    if(args[1].equalsIgnoreCase("accept") && (!(truceMap.get(sender).contains(receiver))) && (!(truceMap.get(receiver).contains(sender))))
-                    {
-                        truceMap.get(sender).add(receiver);
-                        truceMap.get(receiver).add(sender);
-                        sender.sendMessage("You are now in a truce with " + receiver);
-                        receiver.sendMessage("You are now in a truce with " + sender);
-
-                        Inventory senderInv = sender.getInventory();
-                        Inventory receiverInv = receiver.getInventory();
-
-                        ItemStack tpItem = new ItemStack(teleporter);
-                        ItemStack swapItem = new ItemStack(swapPlaces);
-
-                        ItemMeta tpItemMeta = tpItem.getItemMeta();
-                        ItemMeta swapItemMeta = swapItem.getItemMeta();
-
-                        tpItemMeta.setDisplayName(ChatColor.AQUA + "teleporter");
-                        swapItemMeta.setDisplayName(ChatColor.AQUA + "swap");
-
-                        senderInv.addItem(tpItem);
-                        senderInv.addItem(swapItem);
-                        receiverInv.addItem(tpItem);
-                        receiverInv.addItem(swapItem);
-
-                    }
-                    else if(args[1].equalsIgnoreCase("deny"))
-                    {
-                        sender.sendMessage("You will not be in a truce with " + receiver);
-                        receiver.sendMessage("You will not be in a truce with " + sender);
-                    }
-                }
+                playerList.remove(soloPlayer);
             }
             else
             {
-                sender.sendMessage("You must enter a valid player name to truce with them");
+                sender.sendMessage("The solo player is already " + soloPlayer.getName());
+            }
+        }
+ */
+        sender.sendMessage("Entering truce if");
+        if(cmd.getName().equalsIgnoreCase("Truce"))
+        {
+            if(!truceMap.containsKey(sender))
+            {
+                for(int z = 0; z < playerList.size(); z++) {
+                    truceMap.put(playerList.get(z), new ArrayList<>());
+                    cooldownMap.put(playerList.get(z), new ArrayList<>());
+                }
+            }
+            sender.sendMessage("inside if");
+            int playerListSize = playerList.size();
+            sender.sendMessage("hi: " + playerListSize);
+            for(int i = 0; i < playerList.size(); i++)
+            {
+                if(args.length == 0)
+                {
+                    sender.sendMessage("You must enter the name of the player you wish to truce with");
+                }
+                /*
+                else if(args[0].equalsIgnoreCase(soloPlayer.getName()))
+                {
+                    sender.sendMessage("You can't truce with the solo player");
+                }
+
+                 */
+                //sender.sendMessage("inside playerlist for loop");
+                else if(playerList.get(i).getName().equalsIgnoreCase(args[0]) && (!truceMap.get(sender).contains(playerList.get(i))))
+                {
+                    sender.sendMessage("inside truce player name check if");
+                    //int pNum = playerList.indexOf(args[0]);
+                    Player receiver = playerList.get(i);
+                    //Player receiver = playerList.get(pNum);
+
+                    if(args.length < 2)
+                    {
+                        sender.sendMessage("inside if args.length < 2");
+                        receiver.sendMessage(sender.getName() + " wishes to truce with you");
+                        receiver.sendMessage("/truce " + sender.getName() + " accept to accept their request");
+                        receiver.sendMessage("/truce " + sender.getName() + " deny to deny their request");
+                        ArrayList<Player> arrayList = truceMap.get(sender);
+                        arrayList.add(receiver);
+                        truceMap.put(sender, arrayList);
+
+                    }
+                    else if(args.length >= 2)
+                    {
+                        if(args[1].equalsIgnoreCase("accept") /*&& (!(truceMap.get(sender).equals(null))) && (!(truceMap.get(receiver).equals(null)))*/)
+                        {
+                            /*
+                            ArrayList<Player> arrayList = truceMap.get(sender);
+                            arrayList.add(receiver);
+                            truceMap.put(sender, arrayList);
+                            truceMap.get(receiver).add(sender);
+                             */
+                            truceMap.get(sender).add(receiver);
+
+                            cooldownMap.get(sender).add(canTP);
+                            cooldownMap.get(sender).add(canSwap);
+
+                            cooldownMap.get(receiver).add(canTP);
+                            cooldownMap.get(receiver).add(canSwap);
+
+                            sender.sendMessage("You are now in a truce with " + receiver);
+                            receiver.sendMessage("You are now in a truce with " + sender);
+
+                            Inventory senderInv = sender.getInventory();
+                            Inventory receiverInv = receiver.getInventory();
+/*
+                            ItemStack tpItem = new ItemStack(teleporter);
+                            ItemStack swapItem = new ItemStack(swapPlaces);
+
+ */
+
+
+                            ItemMeta tpReceiverItemMeta = teleporterReceiver.getItemMeta();
+                            ItemMeta swapReceiverItemMeta = swapPlacesReceiver.getItemMeta();
+
+                            ItemMeta tpSenderItemMeta = teleporterSender.getItemMeta();
+                            ItemMeta swapSendertItemMeta = swapPlacesSender.getItemMeta();
+
+                            tpReceiverItemMeta.setDisplayName(ChatColor.AQUA + sender.getName() + " teleport");
+                            tpReceiverItemMeta.setLocalizedName("Localized name is teleporter");
+                            teleporterReceiver.setItemMeta(tpReceiverItemMeta);
+
+                            swapReceiverItemMeta.setDisplayName(ChatColor.AQUA + sender.getName() + " swap");
+                            swapReceiverItemMeta.setLocalizedName("localized name is swap");
+                            swapPlacesReceiver.setItemMeta(swapReceiverItemMeta);
+
+                            tpSenderItemMeta.setDisplayName(ChatColor.AQUA + receiver.getName() + " teleport");
+                            tpSenderItemMeta.setLocalizedName("Localized name is teleporter");
+                            teleporterSender.setItemMeta(tpSenderItemMeta);
+
+                            swapSendertItemMeta.setDisplayName(ChatColor.AQUA + receiver.getName() + " swap");
+                            swapSendertItemMeta.setLocalizedName("localized name is swap");
+                            swapPlacesSender.setItemMeta(swapSendertItemMeta);
+
+                            senderInv.addItem(teleporterSender);
+                            senderInv.addItem(swapPlacesSender);
+                            receiverInv.addItem(teleporterReceiver);
+                            receiverInv.addItem(swapPlacesReceiver);
+
+                        }
+                        else if(args[1].equalsIgnoreCase("deny"))
+                        {
+                            truceMap.get(sender).remove(receiver);
+                            //truceMap.get(receiver).remove(sender);
+
+                            sender.sendMessage("You will not be in a truce with " + receiver);
+                            receiver.sendMessage("You will not be in a truce with " + sender);
+                        }
+                    }
+                }
+                else if (truceMap.get(sender).contains(playerList.get(i)))
+                {
+                    sender.sendMessage("Either you are already in a truce with this player or the name is invalid");
+                }
             }
         }
 
@@ -127,35 +238,24 @@ public class TruceFeature implements CommandExecutor
 
         return false;
     }
-
-    //add a cooldown of some sort
-    @EventHandler
-    public void truceTeleport(PlayerInteractEvent e)
-    {
-        ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
-        Player p = e.getPlayer();
-        if(item.equals(teleporter) && e.getAction() == Action.RIGHT_CLICK_AIR)
-        {
-            Location tpLocation = truceMap.get(p).get(0).getLocation();
-            p.teleport(tpLocation);
-        }
-    }
-
-    boolean isSwap = false;
-
+/*
     @EventHandler
     public void truceSwap(PlayerInteractEvent e)
     {
         ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
         Player p = e.getPlayer();
-        if(item.equals(swapPlaces) && e.getAction() == Action.RIGHT_CLICK_AIR)
+        p.sendMessage("Entering if statement");
+        if(item.equals(swapPlaces) && e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_AIR)
         {
+            p.sendMessage("Inside if!!!!");
             Player otherPlayer = truceMap.get(p).get(0).getPlayer();
             otherPlayer.sendMessage(p + " wants to swap places. Right click the swap item to do so.");
             otherPlayer.sendMessage("If you don't want to swap places, left click the swap item");
             isSwap = true;
         }
     }
+
+
 
     int count = 0;
 
@@ -177,7 +277,7 @@ public class TruceFeature implements CommandExecutor
                     Bukkit.getServer().getScheduler().cancelTasks((Plugin) Dontknow.getInstance());
                     count = 0;
                 }
-                else if(item.equals(swapPlaces) && other.getAction() == Action.RIGHT_CLICK_AIR && isSwap == true)
+                else if(item.equals(swapPlaces) && isSwap == true && other.getAction() == Action.RIGHT_CLICK_AIR || other.getAction() == Action.RIGHT_CLICK_AIR)
                 {
                     Location truceLocation = truceMap.get(p).get(0).getLocation();
                     Location pLocation = p.getLocation();
@@ -187,5 +287,7 @@ public class TruceFeature implements CommandExecutor
             }
         }.runTaskTimer((Plugin) Dontknow.getInstance(), 0, 100);
     }
+
+ */
 
 }
